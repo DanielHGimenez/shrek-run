995fba913bc9ce96d6d1a8ba628223949ba6702a
DanielHGimenez <danielhgimenez027@gmail.com>
2024-09-02 02:09:47 +0200

    [API] Fixing retrieve of environment variables

--- api/src/config/env/env.go
+++ api/src/config/env/env.go
@@ -1,9 +1,41 @@
 package env
 
-import "time"
-
-const (
-	WriteTimeout = 10
-	CodeLenght   = 6
-	TimeToStart  = time.Second * 5
+import (
+	"os"
+	"strconv"
+	"time"
 )
+
+var (
+	WriteTimeout uint64 = 10
+	CodeLenght uint64 = 6
+	TimeToStart = time.Second * 5
+	AllowedOrigins = "*"
+)
+
+func Init() {
+	env := os.Getenv("WRITE-TIMEOUT")
+	if env != "" {
+		if v, err := strconv.ParseUint(env, 10, 64); err != nil {
+			WriteTimeout = v
+		}
+	}
+
+	env = os.Getenv("CODE-LENGHT")
+	if env != "" {
+		if v, err := strconv.ParseUint(env, 10, 64); err != nil {
+			CodeLenght = v
+		}
+	}
+
+	env = os.Getenv("TIME-TO-START")
+	if env != "" {
+		if v, err := strconv.ParseInt(env, 10, 64); err != nil {
+			TimeToStart = time.Duration(v) * time.Second
+		}
+	}
+
+	if v := os.Getenv("ALLOWED-ORIGINS"); v != "" {
+		AllowedOrigins = v
+	}
+}
